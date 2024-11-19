254c04d5020114746c18067b807f482dcd041c86
Akshay031-eng <pratapakshay031@gmail.com>
2024-11-19 21:01:15 +0000

    String

---
+++ String.js
@@ -0,0 +1,30 @@
+const name =  "nikhil"
+const no =  40 
+console.log(`i am a ${name} my number is ${no} i live in mohammadpur`)
+
+// and another is this type is give in array form but is not aaray is object 
+
+const gamename = new String('nikhilkumar')
+console.log(gamename [0] )
+console.log(gamename.length)
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(3) )
+console.log(gamename.indexOf('k') )
+
+const newString = gamename.substring(0,4)
+
+console.log(newString)
+
+const anotherString =  gamename.slice(-8,4)
+console.log(anotherString)
+
+const newStringone =  "   akshay     "
+console.log(newStringone)
+console.log(newStringone.trim())
+
+const url =  " hhttp// hnbdshhbd%30nsndh"
+console.log( url.replace('%30' , '-'))
+console.log( url.includes('manu'))
+
+const mil = "manu-manu-tanu"
+console.log(mil.split('-'));
