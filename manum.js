5c0dc15177fb9879277b9649581583ee25d5bb8d
Akshay031-eng <pratapakshay031@gmail.com>
2024-11-24 19:35:27 +0000

    manum.js

---
+++ manum.js
@@ -0,0 +1,19 @@
+const num =  230
+//console.log(num);
+
+const balance =   new   Number (100)
+
+
+ //console.log(balance.toFixed(1))
+
+ const othernum = 234.654
+// console.log(othernum.toPrecision(4))
+
+ const hundered =  10000000
+// console.log(hundered.toLocaleString('en-IN '))
+
+ //***************************   MATHS   */
+ console.log(Math)
+ console.log(Math.abs(-5))
+ console.log(Math.round(5.8))
+
