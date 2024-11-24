300d37eb00b9ebff0109515df891c70d96ccb86e
Akshay031-eng <pratapakshay031@gmail.com>
2024-11-24 20:15:22 +0000

    date.js

---
+++ date.js
@@ -0,0 +1,17 @@
+//DATES ..................
+
+let date = new Date()
+
+console.log(typeof date);
+
+console.log(date);
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toISOString());
+console.log(date.toLocaleDateString());
+console.log(date.toTimeString());
+
+
+
+
+
