a07c61f94dc695f134467544a7a542d4fb2501b4
Akshay031-eng <pratapakshay031@gmail.com>
2024-11-24 20:03:35 +0000

    changes

--- manum.js
+++ manum.js
@@ -16,4 +16,21 @@ const balance =   new   Number (100)
  console.log(Math)
  console.log(Math.abs(-5))
  console.log(Math.round(5.8))
+ console.log(Math.round(5.8));
+ console.log(Math.ceil(5.3));//is basically value is 6
+ console.log(Math.floor(4.8));// is lower 4
+ console.log(Math.random());
+ console.log((Math.random()^10)+1);
+
+  console.log(Math.floor(Math.random()^10)+1);
+// any problem solve like ludoo then number limit is 1 to 6 so use random formula
+  min =1
+  max = 6
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+ 
+
+ 
+ 
+ 
 
