3f21db0480291de3a1f44887a6102173213dd92e
Akshay031-eng <pratapakshay031@gmail.com>
2024-11-25 20:03:03 +0000

    array.js

---
+++ 02basic/array.js
@@ -0,0 +1,46 @@
+///array  **********************
+
+const myarr = [1,7,8,9,0]
+
+const myhero = ["nikhil ", "devansh"]
+const myarr2 = [0,7,6,4,6]
+console.log(myarr[0]);
+
+//////////////////ARRAY METHODS
+
+//myarr.push(6)
+//myarr.pop()
+
+//myarr.unshift(5)// it value add first like 5,1,7,8,9,0
+
+//myarr.shift() it remove first value 7,8,9,0
+//console.log(myarr.includes(9));
+//console.log(myarr.indexOf(19));//-1 because  it is not includes give index value
+
+
+// const newarr = myarr.join()// it array conver and join in Strring is type of is String
+// console.log(myarr);
+// console.log(newarr);
+ 
+
+//********8sli ce ,splice */
+
+console.log("A",myarr);
+ 
+const myn = myarr.slice(1,3)// it only print the 1 and 2 index not includes the last index 1,2 only
+console.log(myn);
+console.log("B", myarr);
+
+const myn1 =  myarr.splice(1,3)///it print all index give jasa 1,2,3
+console.log("C",myarr);// it print only 1,0 because splice then print only left number
+
+console.log(myn1);
+
+
+
+ 
+
+
+
+
+
