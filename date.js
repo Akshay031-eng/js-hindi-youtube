184f2c2b796a375426cb9a8f6b47b55e47d4c58c
Akshay031-eng <pratapakshay031@gmail.com>
2024-11-25 18:52:32 +0000

    changes

--- date.js
+++ date.js
@@ -2,17 +2,33 @@
 
 let date = new Date()
 
-console.log(typeof date);
+//console.log(typeof date);
 
-console.log(date);
-console.log(date.toString());
-console.log(date.toDateString());
-console.log(date.toISOString());
-console.log(date.toLocaleDateString());
-console.log(date.toTimeString());
+//console.log(date);
+//console.log(date.toString());
+//console.log(date.toDateString());
+//console.log(date.toISOString());
+//console.log(date.toLocaleDateString());
+//console.log(date.toTimeString());
 
-let createdate = new Date(4,0,2024)
-console.log(createdate. toDateString());
+let createdate = new Date(2024,0,21) // monthstart in javascript with 0
+//console.log(createdate. toDateString());
+
+// lettime is basically use big project like htel and gor quiz
+
+let timestamp  = Date.now()
+console.log(timestamp);
+console.log(createdate.getTime());
+//for seconds
+
+
+//daates methods like getdate,gettime,getday
+
+let newdate = new Date()
+
+newdate.toLocaleString('default' , {
+   weekday : "long"
+})
 
 
 
