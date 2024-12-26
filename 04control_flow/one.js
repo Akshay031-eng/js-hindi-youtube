85bb025c774f02ac7fb4da6063e4eae4293c5299
Akshay031-eng <pratapakshay031@gmail.com>
2024-12-26 01:07:10 +0000

    one.js

---
+++ 04control_flow/one.js
@@ -0,0 +1,28 @@
+// if
+
+// <,>,==.===,!=,!==,<=,>=
+if(2== "2"){
+    // console.log(`bhanu`) // it excute because in == it equal value 
+    
+}
+if(2=== "2"){
+    console.log(`three equal`) // it excute because in === it equal values and it datatypes of the value
+
+    
+}
+// const temperature =  41
+// if(temperature===50){
+//     console.log(`temperature is less than  50.`);
+// }
+// else{
+//     console.log("temperature is grester than the 50");
+    
+// }
+
+const score  =  200
+if (score>100) {
+    const power ="fly "
+    console.log(`power is `);
+    
+    
+}
